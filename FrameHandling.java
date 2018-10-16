aba0c96d2844d4c9d2658b37e1ddf4760a84a24c
kundan_shriv <kundan_shriv@yahoo.com>
2018-10-16 19:51:47 +0530

    second commit

--- FrameHandling.java
+++ FrameHandling.java
@@ -1,5 +1,7 @@
 package com.basicsdemo.demo;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class FrameHandling {
+	
+	
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
@@ -24,7 +28,7 @@ System.setProperty("webdriver.chrome.driver","C:\\Users\\Achint\\Desktop\\Swagen
 		
 		System.out.println(driver.findElements(By.tagName("frame")).size());
 					
-		driver.switchTo().frame(1);
+		/*driver.switchTo().frame(1);
 		Actions act=new Actions(driver);
 		WebElement contactElement=driver.findElement(By.xpath("//*[text()='Contacts']"));
 		act.moveToElement(contactElement).build().perform();
@@ -33,11 +37,39 @@ System.setProperty("webdriver.chrome.driver","C:\\Users\\Achint\\Desktop\\Swagen
 		
 		driver.findElement(By.xpath("//*[text()='New Contact']")).click();
 		
-		driver.switchTo().defaultContent();
-		
-		
+		driver.switchTo().defaultContent();*/
+		By element=By.xpath("//*[text()='Contacts']");
+		identifyFrame(driver,element);
 
 	
+	}
+	
+	
+	public static void identifyFrame(WebDriver mydriver,By element)
+	{
+		List<WebElement> list=mydriver.findElements(By.tagName("frame"));
+	
+		
+		if(list.size()>0)
+		{
+			for(int i=0;i<list.size();i++)
+			{
+				mydriver.switchTo().frame(i);
+				if(mydriver.findElements(element).size()==1)
+				{
+					mydriver.findElement(element).click();
+					mydriver.switchTo().defaultContent();
+					break;
+				}
+				mydriver.switchTo().defaultContent();
+			}
+			
+			
+		}
+		else
+		{
+			mydriver.findElement(element).click();
+		}
 	}
 
 }
